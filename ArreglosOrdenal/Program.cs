4116942214a63b6fc418da91ac27d0f836f07c12
Eli SsR <suerorodriguezsamuel@gmail.com>
2025-02-16 22:12:52 -0400

    Update Project

--- ArreglosOrdenal/Program.cs
+++ ArreglosOrdenal/Program.cs
@@ -1,26 +1,26 @@
 ﻿using System;
 
-namespace ArreglosOrdenal
+namespace ArreglosOrdenar
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Ordenal numeros del 1 al 10\n");
-            Int64[] OrdenalNumeros = new Int64[10];
+            Int64[] OrdenarNumeros = new Int64[10];
             int cont = 0;
 
-            for (int i = 0; i < OrdenalNumeros.Length; i++)
+            for (int i = 0; i < OrdenarNumeros.Length; i++)
             {
                 Console.Write($"{cont + 1}. Ingrese un numero: ");
                 Int64 NumIngrese = Int64.Parse(Console.ReadLine());
-                OrdenalNumeros[i] = NumIngrese;
+                OrdenarNumeros[i] = NumIngrese;
                 cont++;
             }
 
             Console.WriteLine();
-            Array.Sort(OrdenalNumeros);
-            Console.WriteLine("Números ordenados: " + string.Join(", ", OrdenalNumeros));
+            Array.Sort(OrdenarNumeros);
+            Console.WriteLine("Números ordenados: " + string.Join(", ", OrdenarNumeros));
         }
     }
 }
